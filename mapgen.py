21d82c32635bc5f7fdd5441fb92a2fbbcb0c8b87
thomasm1248 <thomasm1248@gmail.com>
2023-06-22 16:13:48 -0600

    Made map rise in middle and fall off edges

--- mapgen.py
+++ mapgen.py
@@ -5,7 +5,9 @@ import random as r
 # Presets
 size = 100
 scale = 0.2
-maxHeight = 10
+noiseStrength = 5
+baseHeight = 15
+falloffStrength = 0.0085
 numberOfTrees = m.floor(size**2 / 6)
 numberOfShrubs = m.floor(size**2 / 3)
 numberOfHerbs = m.floor(size**2 / 5)
@@ -21,6 +23,10 @@ class v:
 		self.y = y
 	def __str__(self):
 		return '(%d, %d)' % (self.x, self.y)
+def lengthV(a):
+	return (a.x**2 + a.y**2)**.5
+def distV(a, b):
+	return lengthV(subV(a, b))
 def addV(a, b):
 	return v(a.x + b.x, a.y + b.y)
 def subV(a, b):
@@ -42,8 +48,11 @@ def hexCoords(x, y):
 	return addV(scaleV(hexX, x), scaleV(hexY, y))
 def getHeight1(x, y):
 	pos = scaleV(hexCoords(x, y), scale)
-	noise = sn.noise2(pos.x, pos.y)
-	return m.floor((noise + 1)/2 * maxHeight)
+	base = baseHeight - ((size/2-x)**2 + (size/2-y)**2) * falloffStrength
+	rawNoise = sn.noise2(pos.x, pos.y)
+	scaledNoise = rawNoise * noiseStrength
+	totalHeight = base + scaledNoise
+	return m.floor(totalHeight)
 def export():
 	file = open("map.csv", "w")
 	lines = []
@@ -61,32 +70,36 @@ sn.random_seed()
 for x in range(0, size):
 	tiles.append([])
 	for y in range(0, size):
+		terrain = 'grass'
+		height = getHeight1(x, y)
+		if height < 0:
+			height = 0
+			terrain = 'water'
 		tiles[x].append({
-			'type': 'grass',
+			'type': terrain,
 			'data': {},
 			'hiddenData': {},
-			'height': getHeight1(x, y)
+			'height': height
 			})
 for i in range(0, numberOfShrubs):
 	x = r.randint(0, size-1)
 	y = r.randint(0, size-1)
-	tiles[x][y]['type'] = 'shrub'
+	if tiles[x][y]['type'] != 'water':
+		tiles[x][y]['type'] = 'shrub'
 for i in range(0, numberOfHerbs):
 	x = r.randint(0, size-1)
 	y = r.randint(0, size-1)
-	tiles[x][y]['type'] = 'herb'
+	if tiles[x][y]['type'] != 'water':
+		tiles[x][y]['type'] = 'herb'
 for i in range(0, numberOfTrees):
 	x = r.randint(1, size-2)
 	y = r.randint(1, size-2)
+	if tiles[x][y]['type'] == 'water': continue
 	for i in range(0, leavesPerTree):
 		chosenDir = r.choice(directions)
 		leafPos = addV(v(x,y), chosenDir)
-		try:
+		if tiles[leafPos.x][leafPos.y]['type'] != 'water':
 			tiles[leafPos.x][leafPos.y]['type'] = 'undergrowth'
-		except:
-			print(v(x,y));
-			print(chosenDir)
-			print(leafPos);
 	adjacentTree = False
 	for ax in range(-1,2):
 		for ay in range(-1, 2):
